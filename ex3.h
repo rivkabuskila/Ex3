28b7a2dbf8108108e93ca64e33a97335c245475d
rivkabuskila <93676748+rivkabuskila@users.noreply.github.com>
2021-12-20 00:17:54 +0200

    Add files via upload

---
+++ ex3.h
@@ -0,0 +1,8 @@
+#ifndef ex3
+#define ex3
+
+void GemString(char* txt, char* word, int endword, int endtxt);
+void AtbashString(char* str, char* word,int endword, int endtxt);
+void AnagramString(char* str, char* word, int endword, int endtxt);
+
+#endif
