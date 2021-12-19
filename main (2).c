4e70dfabc3cab8a2b4fd4af30252eee66f64a54c
rivkabuskila <93676748+rivkabuskila@users.noreply.github.com>
2021-12-19 13:26:41 +0200

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include "ex3.h"
+int main()
+{
+char txt[1024];
+
+char word[30];
+
+
+
+
+int i=-1;
+do{
+    i++;
+ scanf("%c", &word[i]);
+}while ((word[i]!=' ')&&(word[i]!='\t')&&(word[i]!='\n'));
+
+
+ int j=-1;
+do{
+    j++;
+ scanf("%c", &txt[j]);
+}while ((txt[j]!='~'));
+
+
+  
+
+GemString(txt, word);
+printf("\n");
+AtbashString(txt, word);
+printf("\n");
+ AnagramString(txt,word);
+return 0;
+}
